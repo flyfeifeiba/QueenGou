c8a59fd734401600277c46dd7a107bb9659213a8
yuanyuan.wang <wang15605187510@163.com>
2018-08-06 18:24:34 +0800

    V1

---
+++ QueueGou/src/main/java/com/queue/gou/model/QueueGou.java
@@ -0,0 +1,15 @@
+package com.queue.gou.model;
+
+import com.queue.gou.enums.SexEnum;
+
+public class QueueGou {
+    private SexEnum sex;
+
+    public SexEnum getSex() {
+        return sex;
+    }
+
+    public void setSex(SexEnum sex) {
+        this.sex = sex;
+    }
+}
